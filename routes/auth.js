4be6c86a871d03d891efab09ee37547d8c8c6fa5
ravikrjha20 <jhakrravi20@gmail.com>
2025-02-01 00:47:13 +0530

    some minor changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require("express"); 
 const router = express.Router();
 const authenticateUser = require("../middleware/authentication");
 const {
